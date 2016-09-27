c88f6a73daca334010c486623c720d904f24169d
ChangWon Lee <chiyah92@gmail.com>
2016-09-28 04:01:51 +0900

    Replaced tabs with 4 spaces in .install.sh

--- .install.sh
+++ .install.sh
@@ -3,14 +3,14 @@
 case "$1" in
     brew)
         ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-	brew tap homebrew/bundle
+    brew tap homebrew/bundle
     ;;
     vim)
-		git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+        git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
     ;;
-	ssh)
-		ssh-keygen -t rsa -b 4096 -C "chiyah92@gmail.com"
-		echo "$ cat ~/.ssh/id_rsa.pub"
-		cat ~/.ssh/id_rsa.pub
-	;;
+    ssh)
+        ssh-keygen -t rsa -b 4096 -C "chiyah92@gmail.com"
+        echo "$ cat ~/.ssh/id_rsa.pub"
+        cat ~/.ssh/id_rsa.pub
+    ;;
 esac
