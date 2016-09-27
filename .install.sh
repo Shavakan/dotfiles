6212c9a3f04033f1a6afb267c262a9325429a5b3
ChangWon Lee <chiyah92@gmail.com>
2016-09-28 03:55:30 +0900

    Added ssh-keygen process for Github accounts

--- .install.sh
+++ .install.sh
@@ -6,6 +6,11 @@ case "$1" in
 	brew tap homebrew/bundle
     ;;
     vim)
-	git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+		git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
     ;;
+	ssh)
+		ssh-keygen -t rsa -b 4096 -C "chiyah92@gmail.com"
+		echo "$ cat ~/.ssh/id_rsa.pub"
+		cat ~/.ssh/id_rsa.pub
+	;;
 esac
