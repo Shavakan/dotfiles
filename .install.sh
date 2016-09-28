4bf925230821aefe36c0af409022bb112fc39610
ChangWon Lee <chiyah92@gmail.com>
2016-09-28 17:40:02 +0900

    Added Homebrew bundle install script

--- .install.sh
+++ .install.sh
@@ -4,6 +4,7 @@ case "$1" in
     brew)
         ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
         brew tap homebrew/bundle
+        cd ~ && brew bundle && cd -
     ;;
     vim)
         git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
